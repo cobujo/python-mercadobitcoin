f3db080433ee479eef271fd85f3f42a0112798de
Jacob Araujo <jakeyzack@gmail.com>
2018-05-30 16:20:14 -0700

    removed packages from setup, updated version

--- mercadobitcoin/__init__.py
+++ mercadobitcoin/__init__.py
@@ -3,5 +3,5 @@
 from mercadobitcoin.api import Api
 from mercadobitcoin.trade_api import TradeApi
 
-__author__ = "alfakini"
-__version__ = "0.5.0"
+__author__ = "cobujo"
+__version__ = "0.1"
--- setup.py
+++ setup.py
@@ -29,12 +29,11 @@ INSTALL_REQUIREMENTS = ['requests']
 setup(
     name = 'mercadobitcoin',
     description = 'A Python wrapper for Mercado Bitcoin API',
-    version = '0.5.0',
-    packages = ['mercadobitcoin'],
-    author = 'Alan Fachini',
-    author_email = 'alfakini@gmail.com',
-    url = 'https://github.com/alfakini/python-mercadobitcoin',
-    download_url = 'https://github.com/alfakini/python-mercadobitcoin/tarball/0.5.0',
+    version = '0.1',
+    author = 'cobujo',
+    author_email = 'jakeyzack@gmail.com',
+    url = 'https://github.com/cobujo/python-mercadobitcoin',
+    download_url = 'https://github.com/cobujo/python-mercadobitcoin/archive/master.zip',
     keywords = ['bitcoin', 'litcoin', 'mercadobitcoin', 'trade', 'orderbook'],
     install_requires=INSTALL_REQUIREMENTS,
     classifiers = [
